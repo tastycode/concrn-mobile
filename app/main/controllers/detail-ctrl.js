eb7d3df6c8dd8b588c8cef485a3b26965fc5e5db
tastycode <kultiv8tor@gmail.com>
2015-11-29 21:37:17 -0800

    Add remaining fields to detail form

--- app/main/controllers/detail-ctrl.js
+++ app/main/controllers/detail-ctrl.js
@@ -1,14 +1,40 @@
 angular.module('main')
 .controller('DetailCtrl', function ($scope, $state, $stateParams, ConcrnClient) {
+  $scope.urgencyValues = [
+    {id: 1, label: 'Not urgent'},
+    {id: 2, label: 'This week'},
+    {id: 3, label: 'Today'},
+    {id: 4, label: 'Within an hour'},
+    {id: 5, label: 'Need help now'}
+  ];
+  $scope.genderValues = ['Male', 'Female', 'Other'];
+  $scope.ageValues = ['Youth (0-17)', 'Young Adult (18-34)', 'Adult (35-64)', 'Senior (65+)'];
+  $scope.raceValues = ['Hispanic or Latino',
+                      'American Indian or Alaska Native',
+                      'Asian',
+                      'Black or African American',
+                      'Native Hawaiian or Pacific Islander',
+                      'White',
+                      'Other/Unknown'];
+  $scope.settingValues = ['Public Space', 'Workplace', 'School', 'Home', 'Other'];
+  $scope.observationValues = ['At risk of harm', 'Under the influence', 'Anxious', 'Depressed', 'Aggravated', 'Threatening'];
+
   $scope.report = {
     id: $stateParams.id,
-    photo: null
+    photo: null,
+    urgency: 1,
   };
 
   $scope.finishReport = function () {
     ConcrnClient.updateReport($scope.report.id, {
       report: {
-        image: $scope.report.photo
+        image: $scope.report.photo,
+        observations: $scope.report.observations.join(','),
+        urgency: $scope.report.urgency,
+        age: $scope.report.age,
+        gender: $scope.report.gender,
+        setting: $scope.report.setting,
+        nature: $scope.report.nature
       }
     }).then(function() {
       $state.go('main.map');
